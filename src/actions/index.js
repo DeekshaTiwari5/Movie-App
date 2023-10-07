6d1ecf90589dcce221c2caa8cb02e6b366047d47
unknown <tdeeksha617@gmail.com>
2023-10-07 21:13:29 +0530

    Changing the state to add favourites as well

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,3 +15,16 @@
 //     type:'ADD_MOVIES',
 //     movies:[m1,m2,m3]
 // }
+
+
+
+// action types
+export const ADD_MOVIES = "ADD_MOVIES";
+
+//dispath action creators
+export function addMovies (movies){
+    return{
+        type: ADD_MOVIES,
+        movies
+      }
+}
