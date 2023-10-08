beb7f6782c1a9a389feebe31d707bae6efbabfc6
unknown <tdeeksha617@gmail.com>
2023-10-08 18:40:35 +0530

    Added the functionality to unfavourite movies and changing tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,6 +20,9 @@
 
 // action types
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_TO_FAVOURITE = "ADD_FAVOURITE";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
 
 //dispath action creators
 export function addMovies (movies){
@@ -27,4 +30,23 @@ export function addMovies (movies){
         type: ADD_MOVIES,
         movies
       }
+}
+export function addFavourite (movie){
+  return{
+      type: ADD_TO_FAVOURITE,
+      movie
+    }
+}
+
+export function removeFromFavourites (movie){
+  return{
+      type: REMOVE_FROM_FAVOURITES,
+      movie
+    }
+}
+export function setShowFavourites (val){
+  return{
+      type: SET_SHOW_FAVOURITES,
+      val
+    }
 }
